78137c187f0c48046e441f2c4dd20af64d61e165
GWJYonder <Michael.Dunkel@gmail.com>
2017-12-04 22:21:02 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,86 @@
-creepStatus = require('creepStatus');
+var initialize = require('initialize');
 
-module.exports = {
-    creepMove : function (creep, destination, path){
-        var range = creep.pos.getRangeTo(destination);
-        if (range > 1){
-            if (path && range > 3){
-                creep.moveTo(destination);
+var roomManager = require('roomManager');
+var bucketHelper = require('bucketHelper');
+var squadManager = require('squadManager');
+var creepStatus = require('creepStatus');
+
+module.exports.loop = function () {
+    return;
+    if (!Memory.pause){
+        if (!Memory.initialized){
+            initialize();
+        }
+        
+        for (var i = 0; i < Memory.towers.length; i++)
+        {
+            var tower = Game.getObjectById(Memory.towers[i].towerId);
+            
+            var enemies = tower.pos.findInRange(FIND_HOSTILE_CREEPS, 10);
+            if (enemies && enemies.length){
+                tower.attack(enemies[0]);
+                continue;
             }
-            else{
-                creep.moveByPath(path);
+            var enemy = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+            if (enemy){
+                tower.attack(enemy);
+                continue;
+            }
+            
+            var filterObj = { filter : function(s){ return s.structureType == STRUCTURE_RAMPART && s.hits < 20000; } };
+            var ramparts = tower.pos.findInRange(FIND_MY_STRUCTURES, 10, filterObj);
+            //console.log(ramparts.length);
+            if (ramparts && ramparts.length){
+                tower.repair(ramparts[0]);
+                continue;
+            }
+            var rampart = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, filterObj);
+            if (rampart){
+                tower.repair(rampart);
+                continue;
+            }
+            
+        }
+        
+        for (var creepName in Game.creeps){
+            if(!Memory.creeps[creepName]){
+                Memory.creeps[creepName] = {};
+            }
+            var creep = Game.creeps[creepName];
+            creepStatus.checkStatus(creep);
+        }
+        
+        for (var i = 0; i < Memory.squads.length; i++){
+            squadManager.processSquad(Memory.squads[i]);
+        }
+        
+        for (var spawnName in Game.spawns){
+            var spawn = Game.spawns[spawnName];
+            var creepsToRenew = spawn.pos.findInRange(FIND_MY_CREEPS, 1, {
+                filter : function(c){
+                    return c.memory.status == Memory.statusOptions.NEEDS_RENEW ||
+                    c.memory.status == Memory.statusOptions.IN_RENEW;
+                }});
+            if (creepsToRenew.length){
+                spawn.renewCreep(creepsToRenew[0]);
             }
         }
-        return range;
-    },
-    harvester : function (creep, source, destination, spawn) {
-        //return;
-        //return creep.moveTo(Game.flags['Flag2']);
-        if (creepStatus.isInService(creep)){
-        //if (true){
-        	if(creep.carry.energy < creep.carryCapacity) {
-        		if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-        			creep.moveTo(source);
-        		}			
-        	}
-        	else {
-        		if(creep.transfer(destination, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        			creep.moveTo(destination);
-        		}	
-        		else if (creep.memory.status == Memory.statusOptions.NEEDS_RENEW){
-        		    creep.memory.status = Memory.statusOptions.IN_RENEW;
-        		}
-        	}
-        }
-        else{
-            if (creep.pos.getRangeTo(spawn) > 1){
-                creep.moveTo(spawn);
+        
+        for (var i = 0; i < Memory.roomInfos.length; i++) {
+            var roomInfo = Memory.roomInfos[i];
+            //console.log('room' + i)
+            for(var j = 0; j < roomInfo.claimedSources.length; j++){
+                roomManager.handleClaimedSource(roomInfo.claimedSources[j]);
             }
+            for(var j = 0; j < roomInfo.claimedControllers.length; j++){
+                roomManager.handleClaimedController(roomInfo.claimedControllers[j]);
+            }
+            roomManager.handleGuards(roomInfo);
+            //bucketHelper.processList(roomInfo.bucketInfos);
+            roomManager.handleShuttles(roomInfo);
+            roomManager.handleRepairList(roomInfo);
+            roomManager.handleBuilders(roomInfo);
+            roomManager.manageNeighbors(roomInfo);
         }
-    },
-    upgrader : function (creep, controller, charger) {
-        //return creep.moveTo(Game.flags['Flag2']);
-    	if(creep.carry.energy == 0) {
-    	    var range = creep.pos.getRangeTo(charger);
-    	    if (range < 2){
-    	        if (charger.transfer){
-	                charger.transfer(creep, RESOURCE_ENERGY);
-    	        }
-    	        else{
-    	            charger.transferEnergy(creep);
-    	        }
-    	    }
-    		else {
-    			creep.moveTo(charger);				
-    		}
-    	}
-    	else {
-    		if(creep.upgradeController(controller) == ERR_NOT_IN_RANGE) {
-    			creep.moveTo(controller);				
-    		}
-    	}
-    },
-    shuttle : function (creep, roomInfo, charger) {
-        //return creep.moveTo(Game.flags[roomInfo.standbyFlagName]);
-    	if(creep.carry.energy == 0) {
-    	    if (creep.pos.getRangeTo(charger) > 1){
-    	        creep.moveTo(charger);
-    	    }
-    		else{
-    		    if (charger.transfer){
-    		        charger.transfer(creep, RESOURCE_ENERGY);
-    		    }
-    		    else{
-    		        charger.transferEnergy(creep);
-    		    }
-    		}
-    	}
-    	else {
-    	    var room = Game.rooms[roomInfo.name];
-    	    var emptyStructures = room.find(FIND_MY_STRUCTURES, {
-    	        filter : function(s) { return s.structureType == STRUCTURE_EXTENSION && s.energy < s.energyCapacity;}
-    	        });
-    	    if (!emptyStructures.length){
-    	        emptyStructures = room.find(FIND_MY_STRUCTURES, {
-    	        filter : function(s) { return s.structureType == STRUCTURE_TOWER && s.energy < s.energyCapacity;}
-    	        });
-    	    }
-    	    if (!emptyStructures.length){
-    	        emptyStructures = room.find(FIND_MY_STRUCTURES, {
-    	        filter : function(s) { return s.structureType == STRUCTURE_SPAWN && s != charger && s.energy < s.energyCapacity;}
-    	        });
-    	    }
-    	    if (emptyStructures.length > 0){
-        		if(creep.transferEnergy(emptyStructures[0]) == ERR_NOT_IN_RANGE) {
-        			creep.moveTo(emptyStructures[0]);				
-        		}
-    	    }
-    	    else{
-    	        return creep.moveTo(Game.flags[roomInfo.standbyFlagName]);
-    	    }
-    	}
-	},
+	}
 }
